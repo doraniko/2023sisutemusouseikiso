92cc253dcda2a15404b874b0bcf044185abbe117
doraniko <148538549+doraniko@users.noreply.github.com>
2023-11-10 19:20:54 +0900

    Update 231110 課題１.c

--- 231110 課題１.c
+++ 231110 課題１.c
@@ -6,14 +6,18 @@ int main(void)
 
 	for (int i = 1; i <= max; i++)
 	{
-		if (i % 13 == 0)
+		if (i % 221 == 0)
 		{
-			printf("%d\n", i);
+			printf("%8d\n", i);
 		}
 		else if (i % 17 == 0)
 		{
 			printf("%d\n", i);
 		}
+		else if (i % 13 == 0)
+                {
+	                printf("%d\n", i);
+                }
     }
 
 	return 0;
