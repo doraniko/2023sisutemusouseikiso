8ab1441040a765e9dcae1d9c94e4a4564b42c416
doraniko <148538549+doraniko@users.noreply.github.com>
2024-01-19 19:04:20 +0900

    Update 240119 p232.c

--- 240119 p232.c
+++ 240119 p232.c
@@ -7,20 +7,21 @@ int main(void)
 	double max_weight, min_weight;
 	double weight[3];
 
-	weight[0] = 72.0;
-	weight[1] = 101.5;
-	weight[2] = 52.4;
+	weight[0] = 72.0;  // Aさんの体重
+	weight[1] = 101.5; // Bさんの体重
+	weight[2] = 52.4;  // Cさんの体重
 	
-	max_weight = -DBL_MAX;
-	min_weight = DBL_MAX;
+	max_weight = -DBL_MAX;  // 最大体重の初期化
+	min_weight = DBL_MAX;   // 最小体重の初期化
 
 	for (i = 0; i < 3; i++)
 	{
+		// 最大体重を求めます
 		if (weight[i] >= max_weight)
 		{
 			max_weight = weight[i];
 		}
-
+		// 最小体重を求めます
 		if (weight[i] <= min_weight)
 		{
 			min_weight = weight[i];
